201cd5d10d7d9dc804f370e13cfe85a7b0b41092
mmfk20 <mmfkr9@hotmail.com>
2025-09-07 11:04:31 -0400

    Delete Models directory

--- Models/ApplicationDbContext.cs
+++
@@ -1,9 +0,0 @@
-using System.Data.Entity;
-
-namespace InsuranceMVC.Models
-{
-    public class ApplicationDbContext : DbContext
-    {
-        public DbSet<Insuree> Insurees { get; set; }
-    }
-}
